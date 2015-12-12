111e49fef92eb1f829fbbff3f4db55e8fbf8d3af
SuchMonkey <d.duregger@live.at>
2015-12-12 01:26:57 +0000

    reverting...

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -8,17 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('head').title = `qBert - ${_panel.name}`
 
   // Load all elements
-  _panel.elements.forEach((_element) => loadItem(_element.path))
+  _panel.elements.forEach((_element) => {
+    Polymer.Base.importHref(_element.path, (e) => {
+      console.log('Loading element %o -> successful', e.target.import)
+    }, () => {
+      console.error('Loading element %s -> failed', _element.name)
+    })
+  })
 
   // Load the panel itself and append it to the body
-  loadItem(_panel.path, (import) => document.body.appendChild(import.body))
-
-  function loadItem(href, callback) {
-    Polymer.Base.importHref(_panel.path, (e) => {
-      console.log('Loading item %o -> successful', e.target.import)
-      if(callback) callback(e.target.import)
-    }, () => {
-      console.error('Loading item %s -> failed', _panel.name)
-    })
-  }
+  Polymer.Base.importHref(_panel.path, (e) => {
+    console.log('Loading panel %o -> successful', e.target.import)
+    document.body.appendChild(e.target.import.body)
+  }, () => {
+    console.error('Loading panel %s -> failed', _panel.name)
+  })
 })
